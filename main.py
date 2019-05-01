a46e4fb3b935064cc656e2f90007db4add3e1887
i719184 <Karl.Strauss@voya.com>
2019-05-01 14:30:11 -0400

    refactor to be a class so that we could add a test. We also added a pytest, to show an example

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 """
 A stub for the main entry point into our processing model
 """
-
-def getInputs():
-    # we should probably pull the data from databases
-    return ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
-
-"""
-Computes the model score
-Expects a list of strings
-"""
-def getModelScore(lOfStrings):
-    d =  sum(len(i) for i in lOfStrings)
-    return d/len(lOfStrings) # return mean    
-
-def main():
-    print("hello my big world")
-    print ("The average is ", getModelScore( getInputs()))
+class CMain:    
+    def getInputs(self):
+        # we should probably pull the data from databases
+        return ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+    
+    """
+    Computes the model score
+    Expects a list of strings
+    """
+    def getModelScore(self, lOfStrings):
+        d =  sum(len(i) for i in lOfStrings)
+        return d/len(lOfStrings) # return mean    
+    
+    def main(self):
+        print("hello my big world")
+        print ("The average is ", self.getModelScore( self.getInputs()))
 
 if __name__ == "__main__":
     # execute if run as a script
-    main()
+    CMain().main()
